cfa1e6b5eef6433a90e2cb5d9d4836b6ee2694a6
Gabriel <jgbiel29@gmail.com>
2024-04-24 14:58:52 -0300

    Now able to run executables in the "\bin" folder. This is done using fork and exev. Also did a better parsing function that handles empty space in input strings better. Still need better parsing for tabs and other special characters.

--- wish.c
+++ wish.c
@@ -6,68 +6,122 @@
 #include <sys/wait.h>
 #include <limits.h>
 
-char* parse_string(char** string, char*** ret_array, size_t* size){
-    size_t s = 0;
-    char* ret;
+/*
+The main function is at the bottom
+The code is all contained in this single file.
+*/
+
+//const char *paths[2] = {"/bin/", "/usr/bin/"};
+const char ** paths;
+
+
+/*
+    Parses the string by creating substrings using empty spaces as separators
+
+    Caller should call free on the returned pointer when done with the array
+*/
+char* parse_string(char* string, char*** ret_array, size_t* size){
+
+    size_t s = 0; //size of return array
     char* delim = " ";
-    size_t length = strlen(*string);
+    size_t length = strlen(string);
     char ** ret_arr = *ret_array;
 
-    //Copying string for parsing
-    char* new_string = malloc((length + 1) * sizeof(char));
-    char* free_p = new_string;
-    memcpy(new_string, *string, length + 1);
+    //New write string for parsing
+    char* new_parsed_string = malloc((length + 1) * sizeof(char));
+    char* free_p = new_parsed_string;
 
-    //Finding how many empty spaces we have
+    int delim_flag = 0; //true if we are in an empty space sequence
+    int write_flag = 0; //true if we have already written some char
+    int write_i = 0; //index of the write string
+    
+    //Removing duplicate empty spaces and spaces at begining and end
     for (int i = 0; i < length; ++i){
-        if ( (new_string)[i] == *delim ){
-            if(i == (length - 2)){
+
+        if (string[i] == *delim){
+
+            delim_flag = 1;
+            continue;
+
+        } else {
+
+            if(i == length - 1){
+                //last char is always \n and we dont want it neither the empty space before
+                //so just end it here
                 break;
+
+            } else if (delim_flag == 1) {
+                //This if means we were on an empty space sequence and are now in an letter
+                //write a single empty space to the buffer in place of the whole sequence of spaces
+                if (write_flag){
+                    new_parsed_string[write_i] = ' ';
+                    ++write_i;
+                    s++;
+                }
+                delim_flag = 0;
+
             }
-            s++;
         }
-        if (new_string[i] == '\n'){
-            new_string[i] = ' ';
-        }
+
+        new_parsed_string[write_i] = string[i];
+        write_flag = 1;//write flag is used to stop writing an empty space if we have not written an actual letter before
+        ++write_i;
+
     }
+    new_parsed_string[write_i] = '\0';
     ++s;
 
-    ret_arr = malloc(s * sizeof(long));
-    printf("ret_arr %ld\n", ret_arr);
-    printf("Allocated %d times\n", s);
+    //printf("%s\n", new_parsed_string);
+    //printf("length: %d", strlen(new_parsed_string));
+    //printf("size of s: %d", s);
+    //exit(0);
 
-    //ret = strsep(string, delim);
+    ret_arr = malloc(1 + s * sizeof(long)); //Dont think this buffer is being allocated properly because of s variable
+    //printf("ret_arr %ld\n", ret_arr);
+    //printf("Allocated %d times\n", s);
+
     int position = 0;  
-    while((ret = strsep(&new_string, delim)) != NULL){
-        if(*ret == '\0'){
+    char* current_string;
+
+    //Separating string and setting pointers in array
+    while((current_string = strsep(&new_parsed_string, delim)) != NULL){
+        if(*current_string == '\0'){
             break;
         }
-        *(ret_arr + position) = ret;
+        ret_arr[position] = current_string;
         ++position;
     }
+    ret_arr[s] = NULL;
+
     *size = s;
     *ret_array = ret_arr;
-    return free_p;
+    return free_p; //Freeing this will free all the strings in ret_array
 }
 
-void fork_exec(const char* path){
+//Generates a new process and awaits its return
+void true_fork_exec(char ** array){
     int rc = fork();
     if(rc < 0){
         printf("Fork Failed!\n");
         exit(1);
     } else if (rc == 0){
-        printf("Child process! rc: %d\n", rc);
-        char* arr[] = {"ls", NULL};
-        execv("/bin/ls", arr);
+        //printf("Child process! rc: %d\n", rc);
+        //Maybe a function that checks what needs to be checked here
+        //allocate pointer and concatenate
+        char * const * arr2 = (char* const *)array;
+        char p[500] = {"/bin/"}; //Should check all paths here
+        strcat(p, arr2[0]);
+        execv(p, arr2);
         printf("Error! %d\n", errno);
     } else {
         int rc_wait = wait(NULL);
-        printf("This is the parent rc:%d\n", rc);
+        //printf("This is the parent rc:%d\n", rc);
         return;
     }
-    exit(0);
+    exit(0); //Should never reach here
 }
 
+//Gets the current dir and prints it
 void get_dir(){
     char cwd[PATH_MAX];
     if(getcwd(cwd, sizeof(cwd)) != NULL){
@@ -78,12 +132,17 @@ void get_dir(){
 }
 
 int main(){
-    printf("Welcome to the shell\n");
-    printf("Say something and I will say it back\n");
 
-    size_t size = 2;
-    char* c = (char*) malloc(size * sizeof(char));
+    paths = malloc(sizeof(char**) * 2);
+    paths[0] = "/bin/";
+    paths[1] = "/usr/bin/";
 
+    //Let getline set these
+    size_t size = 0;
+    char* c = NULL;
+
+    //Useful for checking paths later
+    /*
     if(access("/bin/ls", X_OK) == 0){
         printf("Ok it works\n");
     } else {
@@ -91,9 +150,11 @@ int main(){
         int error = errno;
         printf("Error: %d", error);
     }
+    */
 
+    //printf("wish>");
     while(1){
-        printf("wish>");
+        printf("wish> ");
         int ret_size = getline(&c, &size, stdin);
         if(ret_size == -1){
             if(feof(stdin)){
@@ -101,13 +162,12 @@ int main(){
             }
         }
 
-        if(strcmp(c, "exit\n") == 0){
-            return 0;
+        if(strcmp(c, "\n") == 0){
+            continue;
         }
 
-        if(strcmp(c, "fork\n") == 0){
-            fork_exec("");
-            continue;
+        if(strcmp(c, "exit\n") == 0){
+            return 0;
         }
 
         if(strcmp(c, "dir\n") == 0){
@@ -118,16 +178,19 @@ int main(){
         char** ret_array;
         size_t s;
 
-        char* free_p = parse_string(&c, &ret_array, &s);
-        printf("ret_array %ld\n", ret_array);
+        char* free_p = parse_string(c, &ret_array, &s);
+        //printf("ret_array %ld\n", ret_array);
 
-        printf("%s", c);
-        printf("Size is: %d\n", size);
+        //printf("%s", c);
+        //printf("Size is: %d\n", size);
+        true_fork_exec(ret_array);
 
         for (int i = 0; i < s; ++i){
-            printf("%s\n", *(ret_array + i));
+            //printf("%s\n", *(ret_array + i));
         }
         free(free_p);
     }
+
+    //Never reaches here
     return 0;
 }
